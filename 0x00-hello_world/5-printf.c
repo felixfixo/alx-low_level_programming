c675483f56a10aedc7616962153b4fd0f5aa0b69
felixfixo <fixoothina@gmail.com>
2022-04-07 11:45:05 +0300

    latest changes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
