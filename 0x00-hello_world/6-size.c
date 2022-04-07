eb1829482565196f67ba9676a23f1a098e54c3c5
felixfixo <fixoothina@gmail.com>
2022-04-07 11:50:45 +0300

    latest changes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	printf("Size of a char:" typeof(char)\n);
+	return (0);
+}
