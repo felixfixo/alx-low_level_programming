4e6e4722a9f447466751bc7aa2c14d713ece2e01
felixfixo <fixoothina@gmail.com>
2022-04-07 11:29:29 +0300

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 
 /*
  * This is the main function
- */
+*/
 int main(void) 
 {
-	
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
