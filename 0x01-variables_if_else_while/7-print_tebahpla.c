cdfb9f2b3cdbafcd8d53976a029cbe17bb1d9cd7
felixfixo <fixoothina@gmail.com>
2022-04-08 11:31:42 +0300

    question 7 changes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	for (char ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
