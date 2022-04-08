1ee92a7b149a22d591fba8edc734078362abfbf0
felixfixo <fixoothina@gmail.com>
2022-04-08 13:49:36 +0300

    question 8 changes

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,22 +9,23 @@
 int main(void)
 {
 	char base_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 	for (int i = 0; i <  16; i++)
 	{
-		if(base_digits[i] == 'A' || base_digits[i] == 'B' || base_digits[i] == 'C'
+		if (base_digits[i] == 'A' || base_digits[i] == 'B' || base_digits[i] == 'C'
 			|| base_digits[i] == 'D' || base_digits[i] == 'E' || base_digits[i] == 'F')
 		{
 			char lowerCaseLetter = tolower(base_digits[i]);
+
 			putchar(lowerCaseLetter);
 		}
 		else{
 			int hexadecimal =  base_digits[i] % 16;
+
 			putchar(hexadecimal + '0');
 		}
 	}
 	putchar('\n');
-	
 	return (0);
 }
