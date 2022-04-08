18dee0856a81a9f51004b08493b22c4b19a4eed3
felixfixo <fixoothina@gmail.com>
2022-04-08 13:55:28 +0300

    question 8 changes

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,9 @@ int main(void)
 {
 	char base_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
 '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+	int i;
 
-	for (int i = 0; i <  16; i++)
+	for ( i = 0; i <  16; i++)
 	{
 		if (base_digits[i] == 'A' || base_digits[i] == 'B' || base_digits[i] == 'C'
 			|| base_digits[i] == 'D' || base_digits[i] == 'E' || base_digits[i] == 'F')
