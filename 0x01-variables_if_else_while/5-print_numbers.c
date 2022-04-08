e717ee075308bd434f7c2477efbc370a11efcb1f
felixfixo <fixoothina@gmail.com>
2022-04-08 11:16:18 +0300

    question 5 changes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	int number = 0;
+	while (int number < 10)
+	{
+		printf("%d\n", number);
+		number++;
+	}
+	return (0);
+}
