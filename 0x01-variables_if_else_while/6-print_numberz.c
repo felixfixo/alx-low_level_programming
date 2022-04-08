69d4db577d1820ec8988bf2f088403ea74b15ce1
felixfixo <fixoothina@gmail.com>
2022-04-08 11:25:26 +0300

    question 6 changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar(number);
+		putchar('\n');
+		number++;
+	}
+	return (0);
+}
