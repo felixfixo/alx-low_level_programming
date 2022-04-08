70293bc0d460a201b674d33bc770e7e872feb3a8
felixfixo <fixoothina@gmail.com>
2022-04-08 10:55:54 +0300

    question 4 changes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		char capitalLetter = toupper(ch);
+		putchar(capitalLetter);
+		putchar('\n');
+	}
+	return (0);
+}
