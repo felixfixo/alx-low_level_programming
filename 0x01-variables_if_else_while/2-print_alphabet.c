9f5ee6a8c3abab8e8ec932cb02633900c4b52bd7
felixfixo <fixoothina@gmail.com>
2022-04-08 10:42:50 +0300

    third question changes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	for(char ch = a; ch <= z; ch++)
+	{
+		putchar(ch);
+		putchar("\n");
+	}
+	return (0);
+}
