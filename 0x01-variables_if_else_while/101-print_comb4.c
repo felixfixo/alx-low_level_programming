14cc19bf61ce918520b478c22dc16affc8dfcde6
felixfixo <fixoothina@gmail.com>
2022-04-08 22:28:41 +0300

    question 11 edits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	int a;
+	int x;
+	int y
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			for (y = 0; y <= 9; y++)
+			{
+				if (a != x && x != y && a != y && a < x && y > x)
+				{
+					putchar(a + '0');
+					putchar(x + '0');
+					putchar(y + '0');
+					if (!(a == 7 && x == 8 && y == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
