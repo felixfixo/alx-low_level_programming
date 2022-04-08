80875854c0676c1f1fad650638419473da51636a
felixfixo <fixoothina@gmail.com>
2022-04-08 22:33:29 +0300

    question 12 edits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point of execution in C
+ *
+ * Return: Return zero
+ */
+int main(void)
+{
+	int x,
+	int y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 1; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + '0');
+				putchar((x % 10) + '0');
+				putchar(' ');
+				putchar((y / 10) + '0');
+				putchar((y % 10) + '0');
+				if (!(x == 98 && y == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
