9e75c89bd3ea5044b6fd960cb5032b35e5c1165b
felixfixo <fixoothina@gmail.com>
2022-04-12 18:18:35 +0300

    question 12 edits

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,10 +16,8 @@ if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
-
 for (j = 0; j <= n; j++)
 {
-
 if ((i * j) < 10)
 {
 if (j != 0)
@@ -30,7 +28,6 @@ _putchar(' ');
 }
 _putchar((j * i) + '0');
 }
-
 else if ((i * j) < 100)
 {
 _putchar(' ');
@@ -38,7 +35,6 @@ _putchar(' ');
 _putchar((((j * i) / 10) % 10) + '0');
 _putchar(((j * i) % 10) + '0');
 }
-
 else if ((i * j) < 1000)
 {
 _putchar(' ');
