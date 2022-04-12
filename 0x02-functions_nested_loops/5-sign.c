03ac3c592eba933ec7ebb80457b26fdd5b3cbfdc
felixfixo <fixoothina@gmail.com>
2022-04-12 10:58:35 +0300

    question 5 edits.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,12 @@ if (n > 0)
 printf("%+, ", n);
 return (1);
 }
-else if ( n == 0)
+else if (n == 0)
 {
 printf("0, 0");
 return (0);
 }
-else if ( n < 0)
+else if (n < 0)
 {
 printf("%+, ", n);
 return (-1);
