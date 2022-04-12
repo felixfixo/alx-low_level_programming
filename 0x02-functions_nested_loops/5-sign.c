6811c1f419a944fcd3017ec7dd4fafa31226dc5e
felixfixo <fixoothina@gmail.com>
2022-04-12 17:30:12 +0300

    changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("%+, ", n);
+_putchar('+');
 return (1);
 }
 else if (n == 0)
@@ -21,7 +21,7 @@ return (0);
 }
 else if (n < 0)
 {
-printf("%+, ", n);
+_putchar('+');
 return (-1);
 }
 
