4c4f80fc87cd3d974c7072a1e5f74160490e5346
felixfixo <fixoothina@gmail.com>
2022-04-12 18:11:45 +0300

    question 11 edits

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * add - prints all natural numbers from n to 98.
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: The number to be checked
  *
  * Return: void.
  */
