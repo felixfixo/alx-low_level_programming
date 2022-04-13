6910206eb1c4e1d420231638e9b3b2fd3131d1c9
felixfixo <fixoothina@gmail.com>
2022-04-13 23:50:12 +0300

    question 2 changes

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,20 +14,15 @@ int largest;
 
 if (a > b && a > c)
 {
-largest = a;
+   largest = a;
 }
-else if (b > c && b > a)
+else if (b > a && b > c)
 {
 largest = b;
-}
-else if (c > b)
-{
-largest = c;
 }
 else
 {
-largest = b;
+largest = c;
 }
-
 return (largest);
 }
