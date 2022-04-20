3d5843e098711ba9ca46ead64c1533e0e38cec7e
felixfixo <fixoothina@gmail.com>
2022-04-20 23:37:07 +0300

    changes

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - program that generates random valid passwords
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char k[200];
+int num = 0;
+int random = 0;
+char *key = k;
+
+srand(time(NULL));
+
+while (num < 2645)
+{
+random = rand() % 122;
+
+if (random > 32)
+{
+*key = random;
+key = key + 1;
+num += random;
+}
+}
+
+*key = (2772 - num);
+*(key + 1) = '\n';
+printf("%s", k);
+return (0);
+}
