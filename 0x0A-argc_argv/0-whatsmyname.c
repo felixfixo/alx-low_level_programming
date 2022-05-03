92d9f7491c1e681a6951b0475ac1606bb0e7aee7
felixfixo <fixoothina@gmail.com>
2022-05-04 00:18:01 +0300

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -prints its name, followed by a new line.
+ * If you rename the program, it will print the new name,
+ * without having to compile it again
+ * @argc: int
+ * @argv: list
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
