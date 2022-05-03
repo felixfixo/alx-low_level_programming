6fc8664d1ca017459235843ed956c262c17b73fa
felixfixo <fixoothina@gmail.com>
2022-05-04 00:23:36 +0300

    question 1 edits

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * our program should print a number, followed by a new line
+ * @argc: int
+ * @argv: array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
