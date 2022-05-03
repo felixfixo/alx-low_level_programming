74c5b73a72a2328d94aa102db69d186b8d648917
felixfixo <fixoothina@gmail.com>
2022-05-04 00:47:36 +0300

    question 5 edits

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,7 +20,13 @@ return (1);
 }
 
 /**
- * main - adds positive numbers
+ * main - prints the minimum number of coins to make
+ * change for an amount of money.
+ * if the number of arguments passed to your program is not exactly 1,
+ * print Error, followed by a new line, and return 1
+ * You should use atoi to parse the parameter passed to your program
+ * If the number passed as the argument is negative, print 0, followed by a new line
+ * You can use an unlimited number of coins of values 25, 10, 5, 2, and 1 cent
  * @argc: int
  * @argv: list
  * Return: 0
