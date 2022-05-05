6cc4550f5eaf67179b512e584178497b610b4c06
felixfixo <fixoothina@gmail.com>
2022-05-05 17:44:57 +0300

    question 6 edits

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,6 +15,47 @@
  * Return: - char
  */
 
+void performMoreOperations(char *str, int len, char **f, char *col, int count, int i, int j, int k)
+{
+while (*str)
+{
+while (*str == ' ' && *str)
+{
+	str++;
+}
+len = 0;
+
+while (*(str + len) != ' ' && *(str + len))
+{
+len += 1;
+}
+len += 1;
+col = malloc(sizeof(char) * len);
+
+if (!col)
+{
+for (k = j - 1; k >= 0; k--)
+{
+free(f[k]);
+}
+free(f);
+return (NULL);
+}
+
+for (k = 0; k < (len - 1);  k++)
+{
+*(col + k) = *(str++);
+}
+*(col + k) = '\0';
+*(f + j) = col;
+
+if (j < (count - 1))
+{
+j++;
+}
+}
+}
+
 char **strtow(char *str)
 {
 int i = 0, j = 0, k = 0;
@@ -25,6 +66,7 @@ if (!str || !*str)
 {
 return (NULL);
 }
+
 while (*(str + i))
 {
 if (*(str + i) != ' ')
@@ -36,50 +78,21 @@ count += 1;
 }
 i++;
 }
+
 if (count == 0)
 {
 return (NULL);
 }
 count += 1;
 f = malloc(sizeof(char *) * count);
+
 if (!f)
 {
 return (NULL);
 }
 i = 0;
-while (*str)
-{
-while (*str == ' ' && *str)
-{
-str++;
-}
-len = 0;
-while (*(str + len) != ' ' && *(str + len))
-{
-len += 1;
-}
-len += 1;
-col = malloc(sizeof(char) * len);
-if (!col)
-{
-for (k = j - 1; k >= 0; k--)
-{
-free(f[k]);
-}
-free(f);
-return (NULL);
-}
-for (k = 0; k < (len - 1);  k++)
-{
-*(col + k) = *(str++);
-}
-*(col + k) = '\0';
-*(f + j) = col;
-if (j < (count - 1))
-{
-j++;
-}
-}
+performMoreOperations(*str, len, **f,
+	*col, count, i, j, k);
 *(f + j) = NULL;
 return (f);
 }
