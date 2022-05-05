ee9628919c90d08d98f91e36198b4e878a93abb6
felixfixo <fixoothina@gmail.com>
2022-05-05 16:34:34 +0300

    edits

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,8 +5,9 @@
  * _strdup - function that returns a pointer to a newly
  * allocated space in memory, which contains a copy of
  * the string given as a parameter.
- * The _strdup() function returns a pointer to a new string which is a duplicate
- * of the string str. Memory for the new string is obtained with malloc,
+ * The _strdup() function returns a pointer to a new string which
+ * is a duplicate of the string str. Memory for the new string
+ * is obtained with malloc,
  * and can be freed with free.
  * Returns NULL if str = NULL
  * On success, the _strdup function returns a pointer to the duplicated string.
