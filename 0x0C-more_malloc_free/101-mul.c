f9d36dc319beb4a88c009f491e5ad414b36bdcc5
felixfixo <fixoothina@gmail.com>
2022-05-11 01:05:33 +0300

    edits

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * Usage: mul num1 num2
+ * num1 and num2 will be passed in base 10
+ * Print the result, followed by a new line
+ * If the number of arguments is incorrect, print Error,
+ * followed by a new line, and exit with a status of 98
+ * num1 and num2 should only be composed of digits. If not,
+ * print Error, followed by a new line, and exit with a status of 98
+ * You are allowed to use more than 5 functions in your file
+ *
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int i, j;
+if (argc != 3)
+{ 
+printf("Error\n");
+exit(98); 
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{  
+printf("Error\n");
+exit(98); 
+}
+}
+
+}
+mul = atol(argv[1]) *  atol(argv[2]);
+printf("%lu\n", mul);
+return (0);
+}
