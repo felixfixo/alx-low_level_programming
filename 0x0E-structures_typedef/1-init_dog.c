009294baf417682e3ba107a96a09006f0c806f73
felixfixo <fixoothina@gmail.com>
2022-05-12 19:06:52 +0300

    question 1 changes

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * struct dog - dog struct
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner dog
+ * @d: dog struct
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+(*d)name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
+}
