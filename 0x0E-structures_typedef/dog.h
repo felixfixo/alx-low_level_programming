a96ebc66ba3e56b0783bc60e0e5c4b62279c5691
felixfixo <fixoothina@gmail.com>
2022-05-12 19:01:28 +0300

    changes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * struct dog - dog struct
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
