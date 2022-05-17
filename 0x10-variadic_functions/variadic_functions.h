50178434a710553c1fcdfbc5eec91d23d2644801
felixfixo <fixoothina@gmail.com>
2022-05-17 22:14:02 +0300

    betty corrections

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stddef.h>
-#include <stdarg.h> 
-
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-
 void print_int(va_list list);
 void print_float(va_list list);
 void print_char(va_list list);
