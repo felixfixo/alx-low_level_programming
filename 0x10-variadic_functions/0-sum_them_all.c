00f5cd66aafded79919807a37911a40c1c6589f1
felixfixo <fixoothina@gmail.com>
2022-05-17 22:06:15 +0300

    question 0 edits

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
  *@n: integer parameter
  *Return: integer
 */
-int sum_them_all (const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 va_list list;
 unsigned int i, sum = 0;
@@ -17,8 +17,7 @@ unsigned int i, sum = 0;
 va_start(list, n);
 if (n != 0)
 {
-for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
-;
+for (i = 0; i < n; sum += va_arg(list, unsigned int), i++);
 }
 va_end(list);
 return (sum);
