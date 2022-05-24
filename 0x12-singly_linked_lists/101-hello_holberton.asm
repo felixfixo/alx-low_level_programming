67baca1017c3acf9db88fca90ae57220c3c389fb
felixfixo <fixoothina@gmail.com>
2022-05-24 22:07:58 +0300

    question 6 edits

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+; 64-bit program in assembly that prints Hello, Holberton,
+; followed by a new line.
+; You are only allowed to use the printf function
+; You are not allowed to use interrupts
+
+extern printf
+
+section .text
+   global main
+
+main:
+   push rbp
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+   pop rbp
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
