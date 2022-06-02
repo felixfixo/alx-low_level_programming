3967e0b809c9e6ca065082d83fe5fe6caba2bc15
felixfixo <fixoothina@gmail.com>
2022-06-02 16:06:14 +0300

    question 1 edits

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,32 +19,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-if (filename == NULL)
+ssize_t nletters;
+int file;
+
+if (!filename)
+{
+return (1);
+}
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (file == -1)
 {
 return (-1);
 }
-
-int fd;
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-{
-return (-1);
-}
-
 if (text_content)
 {
-int i, readStatus;
-
-for (i = 0; text_content[i] != '\0'; i++)
-	;
-readStatus = write(fd, text_content, i);
-if (readStatus == -1)
+nletters = write(file, text_content, _strlen(text_content));
+if (nletters == -1)
 {
+close(file);
 return (-1);
 }
 }
-close(fd);
+close(file);
 return (1);
 }
